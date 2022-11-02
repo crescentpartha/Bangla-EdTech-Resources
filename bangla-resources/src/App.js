41219e8c6a5e7f53b4372038b79710ca4345342e
crescentpartha <crescentpartha406@gmail.com>
2022-11-02 23:21:23 +0600

    Navbar section added

--- bangla-resources/src/App.js
+++ bangla-resources/src/App.js
@@ -6,6 +6,8 @@ import Header from './components/SharedPages/Header/Header';
 import Footer from './components/SharedPages/Footer/Footer';
 import Register from './components/Authentication/Register/Register';
 import Login from './components/Authentication/Login/Login';
+import PythonHome from './components/Pages/Python/pythonHome/PythonHome';
+import CHome from './components/Pages/C/CHome/CHome';
 
 function App() {
   return (
@@ -14,6 +16,8 @@ function App() {
       <Routes>
         <Route path='/' element={<Home></Home>}></Route>
         <Route path='/home' element={<Home></Home>}></Route>
+        <Route path='/pythonHome' element={<PythonHome></PythonHome>}></Route>
+        <Route path='/cHome' element={<CHome></CHome>}></Route>
         <Route path='/register' element={<Register></Register>}></Route>
         <Route path='/login' element={<Login></Login>}></Route>
         <Route path='*' element={<NotFound></NotFound>}></Route> 
--- bangla-resources/src/components/SharedPages/Header/Header.js
+++ bangla-resources/src/components/SharedPages/Header/Header.js
@@ -1,9 +1,31 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
+import image from '../../../images/logo.png';
 
 const Header = () => {
     return (
         <div>
-            <h2>Header</h2>
+            <Navbar collapseOnSelect expand="lg" sticky="top" bg="light" variant="light">
+                <Container>
+                    <Navbar.Brand as={Link} to="/home" className='d-flex align-items-center gap-2'>
+                        <img src={image} width='30px' alt="logo" />
+                        <span className='' style={{color: '#330662'}}>বাংলা রিসোর্স</span>
+                    </Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="me-auto py-1">
+                            <NavDropdown title="টিউটরিয়াল" id="collasible-nav-dropdown">
+                                <NavDropdown.Item as={Link} to="/pythonHome">পাইথন</NavDropdown.Item>
+                                <NavDropdown.Item as={Link} to="/cHome">সি</NavDropdown.Item>
+                            </NavDropdown>
+                        </Nav>
+                        <Nav>
+                            <Nav.Link className='btn btn-success px-4 text-light' as={Link} to="/login">লগইন</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
         </div>
     );
 };
