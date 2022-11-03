2722a018a01ef2a13121bbc623abd9954e3f959e
crescentpartha <crescentpartha406@gmail.com>
2022-11-04 00:54:31 +0600

    Footer section modified

--- bangla-resources/src/components/SharedPages/Header/Header.js
+++ bangla-resources/src/components/SharedPages/Header/Header.js
@@ -9,7 +9,7 @@ const Header = () => {
             <Navbar collapseOnSelect expand="lg" sticky="top" bg="light" variant="light">
                 <Container>
                     <Navbar.Brand as={Link} to="/home" className='d-flex align-items-center gap-2'>
-                        <img src={image} width='30px' alt="logo" />
+                        <img src={image} width='30px' alt="লগো" />
                         <span className='' style={{color: '#330662'}}>বাংলা রিসোর্স</span>
                     </Navbar.Brand>
                     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
