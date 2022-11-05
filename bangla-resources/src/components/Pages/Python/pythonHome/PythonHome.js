4b009e2f654013cdc2bdc6a692f9cd64ab5a6a51
crescentpartha <crescentpartha406@gmail.com>
2022-11-05 12:19:12 +0600

    CustomLink added

--- bangla-resources/src/components/Pages/Python/pythonHome/PythonHome.js
+++ bangla-resources/src/components/Pages/Python/pythonHome/PythonHome.js
@@ -1,17 +1,18 @@
 import React from 'react';
-import { Link, Outlet } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
+import CustomLink from '../../../SharedPages/CustomLink/CustomLink';
 import './PythonHome.css';
 
 const PythonHome = () => {
     return (
         <div style={{ display: 'grid', gridTemplateColumns: '250px auto' }}>
             {/* <h2>Python Home</h2> */}
-            <div className='navigation pt-3 bg-light position-sticky border-end' style={{ height: '100vh', width: '100%', overflow: 'auto' }}>
-                <nav className='d-flex flex-column flex-wrap text-start'>
+            <div className=' pt-3 bg-light position-sticky border-end' style={{ height: '100vh', width: '100%', overflow: 'auto' }}>
+                <nav className='navigation d-flex flex-column flex-wrap text-start'>
                     <h5 className='py-2 px-3 m-0 fw-semibold text-secondary'>পাইথন টিউটোরিয়াল</h5>
-                    <Link className='py-1 px-3 text-decoration-none text-dark' to='introduction'>ভূমিকা</Link>
-                    <Link className='py-1 px-3 text-decoration-none text-dark' to='getStarted'>এবার শুরু করা যাক</Link>
-                    <Link className='py-1 px-3 text-decoration-none text-dark' to='syntax'>সিনট্যাক্স</Link>
+                    <CustomLink className=' py-1 px-3 d-block' to='introduction'>ভূমিকা</CustomLink>
+                    <CustomLink className='py-1 px-3 d-block' to='getStarted'>এবার শুরু করা যাক</CustomLink>
+                    <CustomLink className='py-1 px-3 d-block' to='syntax'>সিনট্যাক্স</CustomLink>
                 </nav>
             </div>
             <Outlet />
