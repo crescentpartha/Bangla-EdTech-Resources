a9520964f400d8cc6d806cb76996d96ff1c73e4f
crescentpartha <crescentpartha406@gmail.com>
2022-11-22 23:29:40 +0600

    Implement the design of register with Email/Password using react-hooks-form

--- bangla-resources/src/components/Authentication/Register/Register.js
+++ bangla-resources/src/components/Authentication/Register/Register.js
@@ -1,9 +1,69 @@
 import React from 'react';
+import { useForm } from "react-hook-form";
+import { Link } from 'react-router-dom';
 
 const Register = () => {
+    const { register, handleSubmit, formState: { errors } } = useForm();
+
+    const onSubmit = data => {
+        console.log(data);
+    }
+    // console.log(errors);
+
     return (
-        <div>
-            <h2>Register</h2>
+        <div className='w-50 bg-light mx-auto rounded py-4 my-5 shadow-sm'>
+            <h2 className='py-4 text-capitalize'>Create your Account</h2>
+            <form className='d-flex flex-column gap-1 px-5' onSubmit={handleSubmit(onSubmit)}>
+                <input
+                    className='py-2 px-2 mb-2 rounded border-0'
+                    placeholder='Name'
+                    type="text"
+                    {...register("name", {
+                        required: "Name is required",
+                        maxLength: 20,
+                        pattern: {
+                            value: /^[A-Za-z]+$/i,
+                            message: 'Name is invalid'
+                        }
+                    })}
+                />
+                <p className='text-danger'>{errors?.name?.message}</p>
+                <input
+                    className='py-2 px-2 mb-2 rounded border-0'
+                    placeholder='Email'
+                    type="email"
+                    {...register("email", {
+                        required: "Email is required",
+                        pattern: {
+                            value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+                            message: "Email is invalid"
+                        }
+                    })}
+                />
+                <p className='text-danger'>{errors?.email?.message}</p>
+                <input
+                    className='py-2 px-2 mb-2 rounded border-0'
+                    placeholder='Password'
+                    type="password"
+                    {...register("password", {
+                        required: "Password is required",
+                        pattern: {
+                            value: /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{8,20})/,
+                            message: "At least one [a-z][A-Z][0-9][!@#$%^&*] from each categories"
+                        }
+                    })}
+                />
+                <p className='text-danger'>{errors?.password?.message}</p>
+                <input
+                    className='py-2 px-2 rounded bg-primary text-white font-semibold border-0'
+                    type="submit"
+                    value="Register"
+                />
+                <p className='d-flex align-items-center justify-content-between text-primary'>
+                    <Link className='text-decoration-none' to='/login'>Login?</Link>
+                    <Link className='text-decoration-none' to='/retrieve-password'>Forgot Password?</Link>
+                </p>
+            </form>
         </div>
     );
 };
