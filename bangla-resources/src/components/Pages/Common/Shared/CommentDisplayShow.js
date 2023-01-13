a02e8480c036a19f7954d114835a3e4284f8555c
crescentpartha <crescentpartha406@gmail.com>
2023-01-14 01:29:39 +0600

    delete a comment in client-side and send to the server-side

--- bangla-resources/src/components/Pages/Common/Shared/CommentDisplayShow.js
+++ bangla-resources/src/components/Pages/Common/Shared/CommentDisplayShow.js
@@ -12,6 +12,25 @@ const CommentDisplayShow = ({ singleComment }) => {
     const TotalDays = Math.ceil(difference / (1000 * 3600 * 24));
     // console.log(TotalDays);
 
+    const handleDelete = id => {
+        // console.log(id);
+        const proceed = window.confirm('Are you sure?');
+        if (proceed) {
+            // console.log('Deleting comment with id = ', id);
+
+            // delete a comment in client-side and send to the server-side
+            const url = `http://localhost:5000/topic-wise-comment/${id}?tutorial=${singleComment.tutorial}&topic=${singleComment.topic}`;
+            // console.log(url);
+            fetch(url, {
+                method: 'DELETE'
+            })
+            .then(res => res.json())
+            .then(data => {
+                // console.log(data?.deletedCount);
+            });
+        }
+    }
+
     return (
         <div className='mb-3 p-3 shadow-sm rounded'>
             <div className='mb-1 border p-2 rounded'>
@@ -24,6 +43,7 @@ const CommentDisplayShow = ({ singleComment }) => {
             </div>
             <div className='d-flex justify-content-start align-items-center text-decoration-none'>
                 <button
+                    onClick={() => handleDelete(singleComment._id)}
                     className='btn btn-light me-4'
                     style={{ padding: '1px 5px' }}
                 >Delete</button>
