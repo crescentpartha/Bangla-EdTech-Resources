04fded35aa9b1fc5379ced1f975fc40301397fc4
crescentpartha <crescentpartha406@gmail.com>
2023-01-14 00:19:51 +0600

    Modified display comment style and Show the day of comment date

--- bangla-resources/src/components/Pages/Common/Shared/CommentDisplayShow.js
+++ bangla-resources/src/components/Pages/Common/Shared/CommentDisplayShow.js
@@ -4,9 +4,17 @@ const CommentDisplayShow = ({ singleComment }) => {
     const { comment, date, time } = singleComment;
     const arr = comment.split('\n');
     // console.log(arr);
+
+    // Calculate the number of days of comments;
+    const date_1 = new Date(`${date}`);
+    const date_2 = new Date();
+    const difference = Math.abs(date_1.getTime() - date_2.getTime());
+    const TotalDays = Math.ceil(difference / (1000 * 3600 * 24));
+    // console.log(TotalDays);
+
     return (
-        <div className='border p-2 rounded mb-2 px-3'>
-            <div className='mb-1'>
+        <div className='mb-3 p-3 shadow-sm rounded'>
+            <div className='mb-1 border p-2 rounded'>
                 {
                     arr.map((a, index) => <p
                         className='mb-0'
@@ -14,7 +22,17 @@ const CommentDisplayShow = ({ singleComment }) => {
                     >{a}</p>)
                 }
             </div>
-            <p className='mb-0'><small>{date} - {time}</small></p>
+            <div className='d-flex justify-content-start align-items-center text-decoration-none'>
+                <button
+                    className='btn btn-light me-4'
+                    style={{ padding: '1px 5px' }}
+                >Delete</button>
+                <p className='mb-0' title={`${date} at ${time}`}>
+                    <small>
+                        {TotalDays > 1 ? `${TotalDays}d` : 'Today'}
+                    </small>
+                </p>
+            </div>
         </div>
     );
 };
