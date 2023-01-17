49f6c795fb6460e69e63c25579f362590524b7c9
crescentpartha <crescentpartha406@gmail.com>
2023-01-18 01:08:14 +0600

    disable Comment button without Login - Added email, user, and photoURL with comment

--- bangla-resources/src/components/Pages/Common/Shared/CommentInput.js
+++ bangla-resources/src/components/Pages/Common/Shared/CommentInput.js
@@ -2,9 +2,12 @@ import React from 'react';
 import { format } from 'date-fns';
 import { useForm } from "react-hook-form";
 import { toast } from 'react-toastify';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../../../../firebase.init';
 
-const CommentInput = ({tutorial, topic}) => {
+const CommentInput = ({ tutorial, topic }) => {
     const { register, handleSubmit, formState: { errors }, reset } = useForm();
+    const [user] = useAuthState(auth);
     const date = format(new Date(), 'PP'); // uppercase PP
     const time = format(new Date(), 'pp'); // lowercase pp
 
@@ -15,9 +18,13 @@ const CommentInput = ({tutorial, topic}) => {
             topic: topic,
             comment: data.comment,
             date: date,
-            time: time
+            time: time, 
+            email: user.email,
+            user: user.displayName,
+            photoURL: user.photoURL
         }
-        // console.log(data2);
+        console.log(data2);
+        console.log(user);
 
         // POST a comment from client-side to server-side
         const url = `https://bangla-ed-tech-resources-server.vercel.app/comment`;
@@ -28,21 +35,21 @@ const CommentInput = ({tutorial, topic}) => {
             },
             body: JSON.stringify(data2)
         })
-        .then(res => res.json())
-        .then(result => {
-            toast('Your comment is successfully added');
-            // console.log(result);
-            reset();
-        });
+            .then(res => res.json())
+            .then(result => {
+                toast('Your comment is successfully added');
+                // console.log(result);
+                reset();
+            });
     }
 
     return (
         <div className='border px-4 py-3 mt-4 rounded'>
-            <h2 className='fs-3' style={{color: '#04AA6D'}}>Put your Comment</h2>
+            <h2 className='fs-3' style={{ color: '#04AA6D' }}>Put your Comment</h2>
             <form onSubmit={handleSubmit(onSubmit)}>
                 <textarea
                     className='py-2 px-2 mb-2 rounded border block'
-                    style={{ width: '100%', height: '100px'}}
+                    style={{ width: '100%', height: '100px' }}
                     placeholder='Comment...'
                     type="text"
                     {...register("comment", {
@@ -55,12 +62,23 @@ const CommentInput = ({tutorial, topic}) => {
                 />
                 <br />
                 <p className='text-danger'>{errors?.comment?.message}</p>
-                <input
-                    className='py-2 px-3 rounded text-white fw-semibold border-0'
-                    style={{backgroundColor: '#04AA6D'}}
-                    type="submit"
-                    value="Comment"
-                />
+                {
+                    user ?
+                        <input
+                            className='py-2 px-3 rounded text-white fw-semibold border-0 bg-success'
+                            type="submit"
+                            value="Comment"
+                        />
+                        :
+                        <input
+                            className='py-2 px-3 rounded text-white fw-semibold border-0 bg-secondary'
+                            type="submit"
+                            value="Comment"
+                            title="Login your account to comment."
+                            // disabled={!user}
+                            disabled
+                        />
+                }
             </form>
         </div>
     );
