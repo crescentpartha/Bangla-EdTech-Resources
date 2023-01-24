4cc06672283d4c8cc52dc89fec4db29ddf83efc0
crescentpartha <crescentpartha406@gmail.com>
2023-01-24 20:47:12 +0600

    UserInput contents added for Python tutorial

--- bangla-resources/src/components/Pages/Python/Contents/js/Python_UserInput.js
+++ bangla-resources/src/components/Pages/Python/Contents/js/Python_UserInput.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import CommentDisplay from '../../../Common/Shared/CommentDisplay';
 import CommentInput from '../../../Common/Shared/CommentInput';
+import Code from '../../../Common/CodeThemes/Dracula_SyntaxHighlighter';
+
+const code1 = `username = input("Enter username:")
+print("Username is: " + username)`;
+
+const code2 = `username = raw_input("Enter username:")
+print("Username is: " + username)`;
 
 const Python_UserInput = () => {
     return (
@@ -9,10 +16,23 @@ const Python_UserInput = () => {
                 <h1>পাইথন ইউজার ইনপুট</h1> <br />
                 <div>
                     <div>
-
-                    </div>
-                    <div>
-
+                        <h3>ইউজার ইনপুট</h3>
+                        <p>পাইথন ব্যবহারকারীর ইনপুটের জন্য অনুমতি দেয়।</p>
+                        <p>তার মানে আমরা ব্যবহারকারীকে ইনপুট প্রদানের জন্য বলতে পারি।</p>
+                        <p>Python 2.7 এর থেকে Python 3.6-এ পদ্ধতিটি একটু ভিন্ন।</p>
+                        <p>Python 3.6 input() মেথড ব্যবহার করে।</p>
+                        <p>Python 2.7 raw_input() মেথড ব্যবহার করে।</p>
+                        <p>নিম্নলিখিত উদাহরণটি ব্যবহারকারীর নাম জিজ্ঞাসা করে, এবং আপনি যখন ব্যবহারকারীর নাম প্রবেশ করেন, তখন এটি স্ক্রিনে মুদ্রিত হয়।</p>
+                        <div className='clipboard'>
+                            <h4>উদাহরণঃ</h4>
+                            <p>Python 3.6</p>
+                            <Code language='python' code={code1}></Code>
+                        </div>
+                        <div className='clipboard mt-3'>
+                            <h4>উদাহরণঃ</h4>
+                            <p>Python 2.7</p>
+                            <Code language='python' code={code1}></Code>
+                        </div>
                     </div>
                 </div>
                 <div>
