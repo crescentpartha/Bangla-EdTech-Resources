5776ad600d3e820c0245502ce3d2941735c48685
crescentpartha <crescentpartha406@gmail.com>
2023-01-28 23:36:19 +0600

    Functions content added

--- bangla-resources/src/components/Pages/Python/Contents/js/Python_Functions.js
+++ bangla-resources/src/components/Pages/Python/Contents/js/Python_Functions.js
@@ -1,6 +1,22 @@
 import React from 'react';
 import CommentDisplay from '../../../Common/Shared/CommentDisplay';
 import CommentInput from '../../../Common/Shared/CommentInput';
+import Code from '../../../Common/CodeThemes/Dracula_SyntaxHighlighter';
+
+const code1 = `def my_function():
+  print("Hello from a function")`;
+
+const code2 = `def my_function():
+  print("Hello from a function")
+
+my_function()`;
+
+const code3 = `def my_function(fname):
+  print(fname + " Refsnes")
+
+my_function("Emil")
+my_function("Tobias")
+my_function("Linus")`;
 
 const Python_Functions = () => {
     return (
@@ -9,10 +25,15 @@ const Python_Functions = () => {
                 <h1>পাইথন ফাংশন</h1> <br />
                 <div>
                     <div>
-
-                    </div>
-                    <div>
-
+                        <p>একটি ফাংশন হল কোডের একটি ব্লক যা শুধুমাত্র কল করলেই চলে।</p>
+                        <p>আপনি একটি ফাংশনে ডেটা পাস করতে পারেন, যা প্যারামিটার নামে পরিচিত।</p>
+                        <p>একটি ফাংশন ফলাফল হিসাবে ডেটা ফেরত দিতে পারে।</p>
+                        <h3>একটি ফাংশন তৈরি করা</h3>
+                        <p>পাইথনে একটি ফাংশন <mark>def</mark> কীওয়ার্ড ব্যবহার করে সংজ্ঞায়িত করা হয়।</p>
+                        <div className='clipboard'>
+                            <h4>উদাহরণঃ</h4>
+                            <Code language='python' code={code1}></Code>
+                        </div>
                     </div>
                 </div>
                 <div>
